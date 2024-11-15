5759222ad9332d4656cbdadee90aa98524e4bb0f
stammer1 <stammer786@gmail.com>
2024-11-15 11:50:24 -0800

    Backend setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine' , 'ejs');
+
+
+app.get('/' , function(req, res){
+    res.render('index')
+})
+
+app.listen(3000);
