5b2e55120231606a72b9bc843a936786c1f4bc99
stammer1 <stammer786@gmail.com>
2024-11-18 08:34:20 -0800

    Dynamic files handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -9,7 +10,14 @@ app.set('view engine' , 'ejs');
 
 
 app.get('/' , function(req, res){
-    res.render('index')
+    fs.readdir(`./files`, function(err , files){  // Means jab ./files read hojaii phir ham index ko read karai
+        res.render("index" , {files: files} /* this means sending files into index.ejs */)
+    })
+})
+app.post('/create' , function(req , res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt` , req.body.details , function(err){
+        res.redirect("/")
+    });
 })
 
 app.listen(3000);
